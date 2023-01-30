3a63d4a48b401d9dc213960a9c8d3d489fd40b05
maphouse <eehmshaw@gmail.com>
2023-01-30 13:39:34 -0500

    add helper script for installing nunaliit source code

---
+++ additional_helper_scripts/install-nunaliit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ -d "./nunaliit_old" ]
+then
+        echo "removing ./nunaliit_old"
+        rm -r nunaliit_old
+fi
+if [ -d "./nunaliit" ]
+then
+        echo "moving ./nunaliit to ./nunaliit_old"
+        mv nunaliit nunaliit_old
+fi
+git clone https://github.com/GCRC/nunaliit.git uncompiled
+cd uncompiled
+git checkout branch-for-atlascine
+mvn clean install
+#after install, proceed with moving and unpacking executable
+cp nunaliit2-couch-sdk/target/*.tar.gz ../
+cd ..
+tar xf *.tar.gz --transform 's!^[^/]\+\($\|/\)!nunaliit\1!'
+rm nunaliit_*.tar.gz
+rm -rf uncompiled
