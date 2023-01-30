0954ac1614ed930d2bc0d7b4f4cac7f4bdc84241
maphouse <eehmshaw@gmail.com>
2023-01-30 13:59:19 -0500

    improve helper script for installing nunaliit source code

--- additional_helper_scripts/install-nunaliit.sh
+++ additional_helper_scripts/install-nunaliit.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-if [ -d "./nunaliit_old" ]
-then
-        echo "removing ./nunaliit_old"
-        rm -r nunaliit_old
+
+if [ -d "./nunaliit_old" ]; then
+  echo "Removing ./nunaliit_old"
+  rm -rf nunaliit_old
 fi
-if [ -d "./nunaliit" ]
-then
-        echo "moving ./nunaliit to ./nunaliit_old"
-        mv nunaliit nunaliit_old
+
+if [ -d "./nunaliit" ]; then
+  echo "Moving ./nunaliit to ./nunaliit_old"
+  mv nunaliit nunaliit_old
 fi
-git clone https://github.com/GCRC/nunaliit.git uncompiled
-cd uncompiled
-git checkout branch-for-atlascine
-mvn clean install
-#after install, proceed with moving and unpacking executable
-cp nunaliit2-couch-sdk/target/*.tar.gz ../
-cd ..
-tar xf *.tar.gz --transform 's!^[^/]\+\($\|/\)!nunaliit\1!'
-rm nunaliit_*.tar.gz
-rm -rf uncompiled
+
+git clone https://github.com/GCRC/nunaliit.git uncompiled_repo && \
+cd uncompiled_repo && \
+git checkout branch-for-atlascine && \
+mvn clean install && \
+
+cp nunaliit2-couch-sdk/target/*.tar.gz ../ && \
+cd .. && \
+tar xf *.tar.gz --transform 's!^[^/]\+\($\|/\)!nunaliit\1!' && \
+rm -f nunaliit_*.tar.gz && \
+rm -rf uncompiled_repo
