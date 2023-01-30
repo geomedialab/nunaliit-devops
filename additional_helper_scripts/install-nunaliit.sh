a11f621f87111fa72ff223b10e9a580413531948
maphouse <eehmshaw@gmail.com>
2023-01-30 15:44:40 -0500

    revert

--- additional_helper_scripts/install-nunaliit.sh
+++ additional_helper_scripts/install-nunaliit.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 if [ -d "./nunaliit_old" ]; then
-        echo "Removing ./nunaliit_old"
+        echo "removing ./nunaliit_old"
         rm -rf nunaliit_old
 fi
 
 if [ -d "./nunaliit" ]; then
-        echo "Moving ./nunaliit to ./nunaliit_old"
+        echo "moving ./nunaliit to ./nunaliit_old"
         mv nunaliit nunaliit_old
 fi
 
-git clone https://github.com/GCRC/nunaliit.git uncompiled_repo && \
-cd uncompiled_repo && \
+git clone https://github.com/GCRC/nunaliit.git uncompiled && \
+cd uncompiled && \
 git checkout branch-for-atlascine && \
 mvn clean install && \
 
+#after install, proceed with moving and unpacking executable
 cp nunaliit2-couch-sdk/target/*.tar.gz ../ && \
 cd .. && \
 tar xf *.tar.gz --transform 's!^[^/]\+\($\|/\)!nunaliit\1!' && \
-rm -f nunaliit_*.tar.gz && \
-rm -rf uncompiled_repo
+rm nunaliit_*.tar.gz && \
+rm -rf uncompiled
